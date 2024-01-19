67fd910aa891ee0a9bbaa2a45fa58e1ac4b22c15
220825213 <220825213@tut4life.ac.za>
2024-01-19 11:11:26 +0200

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl underline font-bold text-red-500'>Hello world !</div>
+    </>
+  )
+}
+
+export default App
