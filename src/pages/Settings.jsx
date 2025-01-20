699ad201924f9afd32d9933f550dfdc32b88edff
KIPOKA <ladykipoka@gmail.com>
2025-01-20 19:59:44 +0200

    text run 5

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -101,7 +101,7 @@ const Settings = () => {
         {/* Submit Button */}
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full bg-blue-400 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Save Changes
         </button>
