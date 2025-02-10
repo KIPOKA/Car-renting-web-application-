bc5815460bfc4abc8eb2f96887111cdfafacbb7a
KIPOKA <ladykipoka@gmail.com>
2025-02-10 22:24:45 +0200

    Test number 1

--- src/pages/HistoryRides.jsx
+++ src/pages/HistoryRides.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HistoryRides = () => {
-  return <div className="p-4">This is the History Rides page.</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">Ride History</h1>
+      <p>This is the History Rides page.</p>
+    </div>
+  );
 };
 
 export default HistoryRides;
